fba1c938345470679e0400e4cd31f2547991a7bb
John J Schultz <johnmail@bitsofgenius.com>
2021-01-20 11:25:52 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Tee
+{
+	class Program
+	{
+		/// <summary>
+		/// A very simple form of Linux tee.  It will echo standard input to the console, and if an argument is supplied, writes the
+		/// content of standard input to the file name specified AFTER the stdin stream hits EOF.  The output is always written to a
+		/// temp file and the console.  It will rename the temporary file to the namein the first argument before exiting.
+		/// </summary>
+		/// <param name="args">outfile file (optional, but weird to use Tee when not provided)</param>
+		static void Main(string[] args)
+		{
+			try
+			{
+				var tempOutput = Path.GetTempFileName();
+				using (var writer = new StreamWriter(tempOutput))
+				{
+					using (var reader = new StreamReader(Console.OpenStandardInput()))
+					{
+						// Redirect standard output from the console to the output file.
+						/// Console.SetOut(writer);
+						// Redirect standard input from the console to the input file.
+						Console.SetIn(reader);
+						string line;
+						while ((line = Console.ReadLine()) != null)
+						{
+							Console.WriteLine(line);
+							writer.WriteLine(line);
+						}
+					}
+				}
+				if (args.Length >= 1)
+				{
+					if (File.Exists(args[0])) File.Delete(args[0]);
+					File.Copy(tempOutput, args[0]);
+					File.Delete(tempOutput);  // only done when the file name was specified, and it was successfully copied.
+				}
+				Environment.ExitCode = 0;
+			}
+			catch (Exception boom)
+			{
+				Console.WriteLine($"BOOM... there it is: {boom.Message}");
+				Environment.ExitCode = 1;
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Tee
+
+A bare-bones implementation of Linux tee in .NET Core 3.1  Tee provides a way to duplicate standard output to both the console and a file.
+This application was written to provide the most basic, common use of Tee in Windows, without a need to install GNU or other environments.
+
+- **myapplication.exe** would write all output to the console
+- **myapplication.exe > file.txt** would write all output to the file, but nothing to the console.
+
+### Differences from Linux tee
+- It does not support arguments, other than the filename to contain the standard output.
+- If the file exists, the content is overwritten.  Appending to existing output is not supported.
+
+### Usage
+*Tee filename.txt*
+
+Example: **myapplication.exe 2>>&1 | Tee stdout_content.txt**
+
+The above example merges the standard error (2) to standard output, then the pipe routes that output to Tee, which:
+- Creates a temp file
+- Writes the standard output to both the console and the temp file/
+- At the end of the input stream, moves the temp file to the file name in the argument.
+
+### Exit Codes
+- 0 = success
+- 1 = exception detected.
+
+### Installation
+- Ensure .NET Core 3.1 is installed on your system.
+- Change current directory to where the project folder should be cloned
+
+```
+git clone https://github.com/rambotech/Tee.git
+cd Tee
+# change "folder" below to where the binary files should be copied
+dotnet build -c release -o folder
+```
+
+- Add the folder to your path, or use the explicit path in front of the application.
+
+
+
+
+
+
+
+
+
