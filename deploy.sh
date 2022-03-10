70edb1036839be149ec700218edc68abf8a42f4e
John Schultz <johnmail@bitsofgenius.com>
2022-03-10 17:18:00 -0500

    misc fix / Travis-CI build issue fix

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,7 @@ pwd
 
 ls -la
 
-echo NUGET_API_KEY ... contents hidden
-echo NUGET_SOURCE == ${NUGET_SOURCE} 
-echo TRAVIS_PULL_REQUEST == ${TRAVIS_PULL_REQUEST}
-echo BUILD_CONFIG == ${BUILD_CONFIG}
-
-echo BUILD_DIR == ${BUILD_DIR}
+echo dotnet build -c $BUILD_CONFIG Tee
 
 dotnet build -c $BUILD_CONFIG Tee
 
