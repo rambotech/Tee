c0d8d8a531b413d45b3bb556ec079a79fe6b1e08
John J Schultz <rambotech@users.noreply.github.com>
2024-02-13 23:27:20 -0500

    1.0.2: add --append option.  Better argument definitions

--- Program.cs
+++ Program.cs
@@ -1,50 +1,104 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Tee
 {
-	class Program
-	{
-		/// <summary>
-		/// A very simple form of Linux tee.  It will echo standard input to the console, and if an argument is supplied, writes the
-		/// content of standard input to the file name specified AFTER the stdin stream hits EOF.  The output is always written to a
-		/// temp file and the console.  It will rename the temporary file to the namein the first argument before exiting.
-		/// </summary>
-		/// <param name="args">outfile file (optional, but weird to use Tee when not provided)</param>
-		static void Main(string[] args)
-		{
-			try
-			{
-				var tempOutput = Path.GetTempFileName();
-				using (var writer = new StreamWriter(tempOutput))
-				{
-					using (var reader = new StreamReader(Console.OpenStandardInput()))
-					{
-						// Redirect standard output from the console to the output file.
-						/// Console.SetOut(writer);
-						// Redirect standard input from the console to the input file.
-						Console.SetIn(reader);
-						string line;
-						while ((line = Console.ReadLine()) != null)
-						{
-							Console.WriteLine(line);
-							writer.WriteLine(line);
-						}
-					}
-				}
-				if (args.Length >= 1)
-				{
-					if (File.Exists(args[0])) File.Delete(args[0]);
-					File.Copy(tempOutput, args[0]);
-					File.Delete(tempOutput);  // only done when the file name was specified, and it was successfully copied.
-				}
-				Environment.ExitCode = 0;
-			}
-			catch (Exception boom)
-			{
-				Console.WriteLine($"BOOM... there it is: {boom.Message}");
-				Environment.ExitCode = 1;
-			}
-		}
-	}
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// A very simple form of Linux tee.  It will echo standard input to the console, and if an argument is supplied, writes the
+        /// content of standard input to the file name specified AFTER the stdin stream hits EOF.  The output is always written to a
+        /// temp file and the console.  It will rename the temporary file to the namein the first argument before exiting.
+        /// </summary>
+        /// <param name="args">[--append] Outfile</param>
+        /// <remarks>
+        /// outfile is the file name to write the output to.  If --append is present, it is appended to the existing
+        /// Outfile if it exists.  The order of the arguments is irrelevant.
+        /// </remarks>
+        /// <example>c:\temp\myoutput.txt</example>
+        /// <example>c:\temp\myoutput.txt --append</example>
+		/// <example>--append c:\temp\myoutput.txt</example>
+        static void Main(string[] args)
+        {
+            string outputFile = string.Empty;
+            var appendToFile = false;
+            var haveFileName = false;
+            var haveAppendArg = false;
+            var validArguments = true;
+            var index = 0;
+
+            while (index < args.Length)
+            {
+                if (args[index] == "--append")
+                {
+                    if (haveAppendArg)
+                    {
+                        validArguments = false;
+                        Console.WriteLine("argument --append appears more than once");
+                    }
+                    else
+                    {
+                        appendToFile = true;
+                        haveAppendArg = true;
+                    }
+                }
+                else
+                {
+                    if (haveFileName)
+                    {
+                        validArguments = false;
+                        Console.WriteLine("argument for outfile appears more than once.");
+                    }
+                    else
+                    {
+                        haveFileName = true;
+                        outputFile = args[index];
+                    }
+                }
+                index++;
+            }
+
+            if (!haveFileName)
+            {
+                validArguments = false;
+                Console.WriteLine("no argument found for outfile: the  appears more than once.");
+            }
+
+            if (validArguments)
+            {
+                try
+                {
+                    using (var writer = new StreamWriter(outputFile, appendToFile))
+                    {
+                        using (var reader = new StreamReader(Console.OpenStandardInput()))
+                        {
+                            // Redirect standard output from the console to the output file.
+                            /// Console.SetOut(writer);
+                            // Redirect standard input from the console to the input file.
+                            Console.SetIn(reader);
+                            string line;
+                            while ((line = Console.ReadLine()) != null)
+                            {
+                                Console.WriteLine(line);
+                                writer.WriteLine(line);
+                            }
+                        }
+                    }
+                    Environment.ExitCode = 0;
+                }
+                catch (Exception boom)
+                {
+                    Console.WriteLine($"BOOM... there it is: {boom.Message}");
+                    Environment.ExitCode = 1;
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+                Environment.ExitCode = 2;
+            }
+        }
+    }
 }
--- README.md
+++ README.md
@@ -5,10 +5,10 @@
 
 # Tee
 
-A bare-bones implementation of Linux tee in NET 6. Tee provides a way to duplicate standard output to both the console and a file.
+A bare-bones implementation of Linux tee in NET 8. Tee provides a way to duplicate standard output to both the console and a file.
 This application was written to provide the most basic, common use of Tee in Windows, without a need to install GNU or other environments.
 
-- **myapplication.exe** would write all output to the console
+- **myapplication.exe** would write all output to the console.
 - **myapplication.exe > file.txt** would write all output to the file, but nothing to the console.
 
 ### Differences from Linux tee
@@ -16,9 +16,15 @@ This application was written to provide the most basic, common use of Tee in Win
 - If the file exists, the content is overwritten.  Appending to existing output is not supported.
 
 ### Usage
-*Tee filename.txt*
+*Tee filename.txt* or *Tee --append filename.txt*
 
-Example: **myapplication.exe 2>>&1 | Tee file.txt**
+Examples: 
+
+```
+    myapplication.exe 2>>&1 | Tee outputfile.txt
+
+    myapplication.exe 2>>&1 | Tee --append outputfile.txt
+```
 
 The above example merges the standard error (2) to standard output (1), then the pipe routes that output to Tee, which:
 - Creates a temp file
@@ -30,7 +36,7 @@ The above example merges the standard error (2) to standard output (1), then the
 - 1 = exception detected.
 
 ### Installation
-- Ensure NET6 is installed on your system.
+- Ensure .NET 8.0 is installed on your system.
 - Change current directory to where the project folder should be cloned.
 
 ```
@@ -44,8 +50,11 @@ dotnet build -c release -o folder
 
 #### Version History
 
+##### 1.0.2, Feb 13, 2024
+- Change framework to .NET 8.0
+
 ##### 1.0.1, Dec 27, 2021
-- Change framework to NET6
+- Change framework to .NET 6.0
 
 ##### 1.0.0, Jan 20, 2021
 - Original (using .NET Core 3.1)
